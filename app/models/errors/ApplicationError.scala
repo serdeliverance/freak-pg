9823dfa575b51601a2434ebec51e921ae93c387c
Sergio Cano <canosergio90@gmail.com>
2021-01-06 09:39:31 -0300

    circe config and application error model (#5)

---
+++ app/models/errors/ApplicationError.scala
@@ -0,0 +1,15 @@
+package models.errors
+
+import models.validations.ValidationErrorCode
+
+trait ApplicationError
+
+case class InvalidParameterError(code: ValidationErrorCode, parameter: String)
+
+case object EmptyResponse extends ApplicationError
+
+case class DataBaseError(msg: String) extends ApplicationError
+
+case class UnexpectedError(throwable: Throwable) extends ApplicationError
+
+case class ClientError(errorMessage: String) extends ApplicationError
